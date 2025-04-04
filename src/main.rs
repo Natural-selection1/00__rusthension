7604576b5ee6c9a023f9886dc5e6d3b541d7bfd7
Hegui Dai <natural_selection_@outlook.com>
2025-04-04 11:36:31 +0800

    添加了对HashMap、HashSet、VecDeque的支持

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use rusthension::{
-    b_tree_map_comprehension, b_tree_set_comprehension,
-    binary_heap_comprehension, linked_list_comprehension, vec_comprehension,
+    b_tree_map, b_tree_set, binary_heap, hash_map, hash_set, linked_list, vec_,
+    vec_deque,
 };
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, LinkedList};
+use std::collections::{
+    BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque,
+};
 
 fn main() {
     test_vec();
@@ -10,13 +12,16 @@ fn main() {
     test_linked_list();
     test_b_tree_set();
     test_b_tree_map();
+    test_vec_deque();
+    test_hash_set();
+    test_hash_map();
 }
 
 fn test_vec() {
     {
         println!("测试返回元组_1迭代器_有条件");
         let vec = vec![("a", 1), ("b", 2), ("c", 3)];
-        let result = vec_comprehension![(x, y) for (x, y) in vec if y >= 2];
+        let result = vec_![(x, y) for (x, y) in vec if y >= 2];
         assert_eq!(result, [("b", 2), ("c", 3)]);
     }
 
@@ -27,12 +32,12 @@ fn test_vec() {
             ("b".to_string(), 2),
             ("c".to_string(), 3),
         ];
-        let result = vec_comprehension![(x, y) for (x, y) in vec if y >= 2];
+        let result = vec_![(x, y) for (x, y) in vec if y >= 2];
 
         assert_eq!(result, [("b".to_string(), 2), ("c".to_string(), 3)]);
     }
     {
-        let result = vec_comprehension![
+        let result = vec_![
             [x, y] if x > y else [y, x]
             for x in (0..y+2) if x % 2 == 0
             for y in 0..7 if y % 3 == 0
@@ -56,49 +61,63 @@ fn test_vec() {
         let vec_comprehension1 = vec![("a", 1), ("b", 2), ("c", 3)];
         let vec_comprehension2 = vec![("a", 1), ("b", 2), ("c", 3)];
         let vec_comprehension3 = vec![("a", 1), ("b", 2), ("c", 3)];
-        let result = vec_comprehension![
+        let _result = vec_![
             y if x > *z else y
             for x in vec_comprehension1.clone()
             for y in vec_comprehension2
             for z in &vec_comprehension3
         ];
-
-        println!("{:?}", vec_comprehension1);
-        // println!("{:?}", vec_comprehension2);
-        println!("{:?}", result);
     }
 }
 
 fn test_binary_heap() {
     let vec = vec![1, 2, 3];
-    let result = binary_heap_comprehension![x for x in vec];
+    let result = binary_heap![x for x in vec];
     assert_eq!(result.into_sorted_vec(), vec![1, 2, 3]);
 }
 
 fn test_linked_list() {
     let vec = vec![1, 2, 3];
-    let result = linked_list_comprehension![x for x in vec];
+    let result = linked_list![x for x in vec];
     assert_eq!(result, LinkedList::from([1, 2, 3]));
 }
 
 fn test_b_tree_set() {
     let vec = vec![1, 2, 3];
-    let result = b_tree_set_comprehension![x for x in vec];
+    let result = b_tree_set![x for x in vec];
     assert_eq!(result, BTreeSet::from([1, 2, 3]));
 }
 
 fn test_b_tree_map() {
-    let vec_key = vec![("a", 1), ("b", 2), ("c", 3)];
-    let vec_value = vec![("a", 1), ("b", 2), ("c", 3)];
+    let vec_key = vec!["key_1", "key_2", "key_3"];
+    let vec_value = [1, 2, 3];
 
-    let result =
-        b_tree_map_comprehension![x , y for x in vec_key for y in vec_value];
+    let result = b_tree_map![x , y for y in vec_value for x in vec_key];
     assert_eq!(
         result,
-        BTreeMap::from([
-            (("a", 1), ("a", 1)),
-            (("b", 2), ("b", 2)),
-            (("c", 3), ("c", 3))
-        ])
+        BTreeMap::from([("key_1", 3), ("key_2", 3), ("key_3", 3)])
+    );
+}
+
+fn test_vec_deque() {
+    let vec = vec![1, 2, 3];
+    let result = vec_deque![x for x in vec];
+    assert_eq!(result, VecDeque::from([1, 2, 3]));
+}
+
+fn test_hash_set() {
+    let vec = vec![1, 2, 3];
+    let result = hash_set![x for x in vec];
+    assert_eq!(result, HashSet::from([1, 2, 3]));
+}
+
+fn test_hash_map() {
+    let vec_key = vec!["key_1", "key_2", "key_3"];
+    let vec_value = [1, 2, 3];
+
+    let result = hash_map![x , y for y in vec_value for x in vec_key];
+    assert_eq!(
+        result,
+        HashMap::from([("key_1", 3), ("key_2", 3), ("key_3", 3)])
     );
 }
